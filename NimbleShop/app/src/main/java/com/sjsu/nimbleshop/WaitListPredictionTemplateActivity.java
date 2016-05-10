43ed903aad10c4aebd6f7fbaa96a3f688dfbb6ba
Sai Repaka <srepaka@8x8.com>
2016-05-10 00:37:25 -0700

    local

--- NimbleShop/app/src/main/java/com/sjsu/nimbleshop/WaitListPredictionTemplateActivity.java
+++ NimbleShop/app/src/main/java/com/sjsu/nimbleshop/WaitListPredictionTemplateActivity.java
@@ -1,39 +1,37 @@
 package com.sjsu.nimbleshop;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.sjsu.nimbleshop.Vo.DataWrapper;
 import com.sjsu.nimbleshop.Vo.StoreVo;
 import com.sjsu.nimbleshop.Vo.TemplateVo;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,6 +41,7 @@ import java.util.Map;
 
 public class WaitListPredictionTemplateActivity extends AppCompatActivity {
     ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<LatLng> locations = new ArrayList();
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -53,8 +52,13 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
     Map<Integer, StoreVo> mapStores = new HashMap<>();
     List<Integer> listStores = new ArrayList<Integer>();
     Map<Integer, List<RadioButton>> mapGroupRadio = new LinkedHashMap<>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
 
-    public void initTable( List<TemplateVo> listTemplate ){
+    public void initTable(List<TemplateVo> listTemplate) {
 
         TableLayout tableLayout = (TableLayout) findViewById(R.id.tableTemplate);
 
@@ -62,35 +66,34 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
         for (int i = 0; i < listTemplate.size(); i++) {
 
             TemplateVo templateVo = listTemplate.get(i);
-            System.out.println( "Name " + templateVo.getName() );
-            if( null != templateVo.getStores() && !templateVo.getStores().isEmpty() ){
+            System.out.println("Name " + templateVo.getName());
+            if (null != templateVo.getStores() && !templateVo.getStores().isEmpty()) {
 
                 TableRow tbrow = new TableRow(this);
-                TextView tv =  new TextView(this);
-                tv.setText( templateVo.getName() );
+                TextView tv = new TextView(this);
+                tv.setText(templateVo.getName());
                 tbrow.addView(tv);
                 tableLayout.addView(tbrow);
                 //hh
                 boolean isNa = false;
 
-                mapGroupRadio.put( templateVo.getTemplateId(), new ArrayList<RadioButton>() );
+                mapGroupRadio.put(templateVo.getTemplateId(), new ArrayList<RadioButton>());
 
 
-                for(StoreVo storeVo : templateVo.getStores() ){
+                for (StoreVo storeVo : templateVo.getStores()) {
 
-                    if( null != storeVo.getListProducts() && !storeVo.getListProducts().isEmpty() ){
+                    if (null != storeVo.getListProducts() && !storeVo.getListProducts().isEmpty()) {
 
                         TableRow tbr = new TableRow(this);
 
                         RadioButton rd = new RadioButton(this);
-                        StringBuffer sb = new StringBuffer( "" );
-                        sb.append(templateVo.getTemplateId() );
+                        StringBuffer sb = new StringBuffer("");
+                        sb.append(templateVo.getTemplateId());
                         sb.append("00");
-                        sb.append( storeVo.getStoreId() );
-                        rd.setId( Integer.parseInt(sb.toString()) );
+                        sb.append(storeVo.getStoreId());
+                        rd.setId(Integer.parseInt(sb.toString()));
 
-                        mapGroupRadio.get( templateVo.getTemplateId() ).add( rd );
-
+                        mapGroupRadio.get(templateVo.getTemplateId()).add(rd);
 
 
                         rd.setOnClickListener(new View.OnClickListener() {
@@ -98,11 +101,11 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
                             public void onClick(View v) {
                                 RadioButton rb = (RadioButton) v;
                                 int wholeId = rb.getId();
-                                String sTempId = String.valueOf(wholeId).substring( 0, String.valueOf(wholeId).lastIndexOf("00") );
+                                String sTempId = String.valueOf(wholeId).substring(0, String.valueOf(wholeId).lastIndexOf("00"));
                                 int tempId = Integer.parseInt(sTempId);
 
-                                for( RadioButton rdb : mapGroupRadio.get(tempId) ){
-                                    if(wholeId == rdb.getId())
+                                for (RadioButton rdb : mapGroupRadio.get(tempId)) {
+                                    if (wholeId == rdb.getId())
                                         rdb.setChecked(true);
                                     else
                                         rdb.setChecked(false);
@@ -114,7 +117,7 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
                         tbr.addView(rd);
 
                         TextView tv1 = new TextView(this);
-                        tv1.setText( storeVo.getStoreName() );
+                        tv1.setText(storeVo.getStoreName());
                         //tv1.setTextColor(Color.BLACK);
                         tbr.addView(tv1);
 
@@ -129,26 +132,25 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
                         tbr.addView(tv5);
 
                         TextView tv2 = new TextView(this);
-                        tv2.setText( String.valueOf( storeVo.getListProducts().get(0).getCost()) );
+                        tv2.setText(String.valueOf(storeVo.getListProducts().get(0).getCost()));
                         //tv2.setTextColor(Color.BLACK);
                         tbr.addView(tv2);
 
                         TextView tv3 = new TextView(this);
-                        tv3.setText( String.valueOf(storeVo.getListProducts().get(0).getQuantity()) );
+                        tv3.setText(String.valueOf(storeVo.getListProducts().get(0).getQuantity()));
                         //tv3.setTextColor(Color.BLACK);
                         tbr.addView(tv3);
 
                         tableLayout.addView(tbr);
-                    }
-                    else{
+                    } else {
                         isNa = true;
                     }
                 }
 
-                if( isNa ){
+                if (isNa) {
                     TableRow tbr = new TableRow(this);
                     TextView tv0 = new TextView(this);
-                    tv0.setText( "NA" );
+                    tv0.setText("NA");
                     //tv0.setTextColor(Color.BLACK);
                     //tv0.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
                     tbr.addView(tv0);
@@ -161,27 +163,27 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
             }
 
 
-
         }
 
 
-        Button navBtn= new Button(this);
+        Button navBtn = new Button(this);
         navBtn.setText("Continue");
         //navBtn.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
         navBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                for( Map.Entry<Integer, List<RadioButton>> entry : mapGroupRadio.entrySet() ){
+
+                for (Map.Entry<Integer, List<RadioButton>> entry : mapGroupRadio.entrySet()) {
 
                     int templateId = entry.getKey();
                     int storeId = 0;
-                    for( RadioButton rb : entry.getValue() ){
-                        if( rb.isChecked() ){
-                            storeId = Integer.parseInt( String.valueOf(rb.getId()).substring( String.valueOf(rb.getId()).lastIndexOf("00") ) );
+                    for (RadioButton rb : entry.getValue()) {
+                        if (rb.isChecked()) {
+                            storeId = Integer.parseInt(String.valueOf(rb.getId()).substring(String.valueOf(rb.getId()).lastIndexOf("00")));
                             //storeId = rb.getId()%10;
                             listStores.add(storeId);
-                            System.out.println(" Chosen store  : " + templateId +  " -- " + storeId );
+                            System.out.println(" Chosen store  : " + templateId + " -- " + storeId);
                             break;
                         }
 
@@ -190,14 +192,30 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
 
                 System.out.println("----------------------------------------");
                 System.out.println("Addresses to go to.");
-                for( Integer i : listStores ){
-                    System.out.println(  mapStores.get(i).getAddress().getLatitude() + "," + mapStores.get(i).getAddress().getLongitude()  );
+                for (Integer i : listStores) {
+                    System.out.println(mapStores.get(i).getAddress().getLatitude() + "," + mapStores.get(i).getAddress().getLongitude());
+                    locations.add(new LatLng(mapStores.get(i).getAddress().getLatitude(), mapStores.get(i).getAddress().getLongitude()));
                 }
+
+                gotoTemplate(locations);
             }
         });
 
         tableLayout.addView(navBtn);
     }
+
+    public void gotoTemplate(ArrayList<LatLng> locations)
+    {
+        Intent intent = new Intent(this, LocationTemplate.class);
+        intent.putParcelableArrayListExtra("locations", locations);
+        startActivity(intent);
+    };
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -212,41 +230,71 @@ public class WaitListPredictionTemplateActivity extends AppCompatActivity {
             obj = new JSONArray(response);
             Log.d("My App", obj.toString());
 
-            listTemplate = new Gson().fromJson(response, new TypeToken<List<TemplateVo>>(){}.getType());
+            listTemplate = new Gson().fromJson(response, new TypeToken<List<TemplateVo>>() {
+            }.getType());
 
-            for( TemplateVo t : listTemplate ){
-                if( null != t.getStores() ){
-                    for( StoreVo storeVo : t.getStores() ){
-                        mapStores.put( storeVo.getStoreId(), storeVo );
+            for (TemplateVo t : listTemplate) {
+                if (null != t.getStores()) {
+                    for (StoreVo storeVo : t.getStores()) {
+                        mapStores.put(storeVo.getStoreId(), storeVo);
                     }
                 }
             }
-            System.out.println( " Kandarp " + listTemplate.size() ) ;
+            System.out.println(" Kandarp " + listTemplate.size());
 
         } catch (Throwable t) {
             Log.e("My App", "Could not parse malformed JSON");
         }
 
         // Populating tables
-        initTable( listTemplate );
+        initTable(listTemplate);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void mapview(View view) {
 
 
-           // ArrayList from = (ArrayList) listValue.get(3);
-            //ArrayList toLoctn = (ArrayList) listValue.get(4);
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "WaitListPredictionTemplate Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.sjsu.nimbleshop/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client2, viewAction);
+    }
 
-            String uri = "http://maps.google.com/maps?f=d&hl=en&saddr=" + 37.345185 + "," + -121.936453 + "&daddr=" + 37.33819 + "," + -121.884142 + "&daddr=" + 37.33868 + "," + -121.904431;
-            System.out.println(uri);
-            //System.out.println(from.get(0).getClass().getName());
-            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-            i.setClassName("com.google.android.apps.maps",
-                    "com.google.android.maps.MapsActivity");
-            startActivity(i);
-        }
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "WaitListPredictionTemplate Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.sjsu.nimbleshop/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client2, viewAction);
+        client2.disconnect();
+    }
 
 //    @Override
 //    public void onStart() {
